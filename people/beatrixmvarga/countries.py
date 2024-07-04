e1298d046ce45bcb255f158c96219f35624ef440
The Map Group <admin@the-map-group.top>
2024-07-04 03:35:11 -0300

    [auto] Updated map for member 'Beatrix MK'

--- people/beatrixmvarga/countries.py
+++ people/beatrixmvarga/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'AT': ['Austria', 123, 123],
   'IT': ['Italy', 9, 9],
-  'US': ['United States of America', 470, 472],
+  'CA': ['Canada', 2, 2],
+  'US': ['United States of America', 471, 473],
   'HU': ['Hungary', 81, 82],
-  'CA': ['Canada', 2, 2]
+  'AT': ['Austria', 123, 123]
 }
