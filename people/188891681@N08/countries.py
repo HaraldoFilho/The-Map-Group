03f03a48e3ef119b503f9eaa4c0e05d42aad8c64
The Map Group <admin@the-map-group.top>
2024-07-06 04:17:29 -0300

    [auto] Updated map for member 'kundun59'

--- people/188891681@N08/countries.py
+++ people/188891681@N08/countries.py
@@ -1,10 +1,13 @@
 countries_dict = {
-  'GR': ['Greece', 15, 20],
-  'US': ['United States of America', 7, 7],
-  'GB': ['United Kingdom', 1, 1],
-  'FR': ['France', 4, 4],
-  'SE': ['Sweden', 1, 1],
-  'HU': ['Hungary', 1, 1],
   'IT': ['Italy', 4, 4],
-  'JP': ['Japan', 2, 2]
+  'MX': ['Mexico', 1, 1],
+  'SE': ['Sweden', 1, 1],
+  'GB': ['United Kingdom', 1, 1],
+  'US': ['United States of America', 7, 7],
+  'GR': ['Greece', 15, 20],
+  'BE': ['Belgium', 1, 1],
+  'HU': ['Hungary', 1, 1],
+  'JP': ['Japan', 2, 2],
+  'EG': ['Egypt', 1, 1],
+  'FR': ['France', 4, 4]
 }
