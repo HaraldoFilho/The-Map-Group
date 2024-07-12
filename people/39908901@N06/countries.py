777e4daa4fff369fa8129864ef4dccc97bd59b64
The Map Group <admin@the-map-group.top>
2024-07-12 03:27:37 -0300

    [auto] Updated map for member 'm01229'

--- people/39908901@N06/countries.py
+++ people/39908901@N06/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'TH': ['Thailand', 12, 82],
-  'TZ': ['Tanzania', 163, 489],
-  'US': ['United States of America', 8756, 28350],
   'IS': ['Iceland', 415, 1179],
-  'IN': ['India', 142, 731],
-  'NL': ['The Netherlands', 15, 37],
-  'MV': ['Maldives', 16, 58],
-  'PK': ['Pakistan', 2, 7],
-  'CA': ['Canada', 563, 1133],
+  'TZ': ['Tanzania', 163, 489],
+  'US': ['United States of America', 8765, 28390],
+  'PT': ['Portugal', 121, 489],
   'KE': ['Kenya', 360, 779],
-  'ES': ['Spain', 14, 42],
-  'FR': ['France', 2, 7],
   'UG': ['Uganda', 114, 239],
+  'IN': ['India', 142, 731],
   'LK': ['Sri Lanka', 21, 109],
-  'PT': ['Portugal', 121, 489]
+  'TH': ['Thailand', 12, 82],
+  'CA': ['Canada', 563, 1133],
+  'MV': ['Maldives', 16, 58],
+  'ES': ['Spain', 14, 42],
+  'PK': ['Pakistan', 2, 7],
+  'NL': ['The Netherlands', 15, 37],
+  'FR': ['France', 2, 7]
 }
