ded18e96fc349c3de4a1fa05f6842bcd226796b3
The Map Group <admin@the-map-group.top>
2024-07-17 03:27:59 -0300

    [auto] Updated map for member 'CrËOS Photographie'

--- people/scfoto62/countries.py
+++ people/scfoto62/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'BE': ['Belgium', 83, 157],
-  'FR': ['France', 1783, 2826],
-  'DE': ['Germany', 2, 2]
+  'DE': ['Germany', 2, 2],
+  'FR': ['France', 1785, 2828],
+  'BE': ['Belgium', 83, 157]
 }
--- people/scfoto62/user.py
+++ people/scfoto62/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Sébastien Croës',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/51101129@N08_r.jpg',
   'url': 'https://www.flickr.com/photos/scfoto62/',
-  'markers': 1868,
-  'photos': 2985
+  'markers': 1870,
+  'photos': 2987
 }
