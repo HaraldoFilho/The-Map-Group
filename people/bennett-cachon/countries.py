b7bc5476da3381506099aea1a4b3d5773e5034d2
The Map Group <admin@the-map-group.top>
2024-07-23 03:26:28 -0300

    [auto] Updated map for member 'Gérard Cachon'

--- people/bennett-cachon/countries.py
+++ people/bennett-cachon/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'NL': ['Netherlands', 125, 193],
-  'IS': ['Iceland', 21, 28],
-  'NZ': ['New Zealand', 18, 19],
   'US': ['United States of America', 110, 130],
+  'NZ': ['New Zealand', 18, 19],
+  'NL': ['Netherlands', 125, 194],
   'CL': ['Chile', 8, 18],
-  'CA': ['Canada', 5, 7]
+  'CA': ['Canada', 5, 7],
+  'IS': ['Iceland', 21, 28]
 }
