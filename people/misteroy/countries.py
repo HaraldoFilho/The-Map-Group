de13ebef263c2e19f70bd7404a8d760cf2ac47ee
The Map Group <admin@the-map-group.top>
2024-07-24 03:41:21 -0300

    [auto] Updated map for member 'Mister Oy'

--- people/misteroy/countries.py
+++ people/misteroy/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'GB': ['United Kingdom', 4215, 6025]
+  'GB': ['United Kingdom', 4216, 6026]
 }
--- people/misteroy/user.py
+++ people/misteroy/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Dave Green',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/7126815@N05_r.jpg',
   'url': 'https://www.flickr.com/photos/misteroy/',
-  'markers': 4215,
-  'photos': 6025
+  'markers': 4216,
+  'photos': 6026
 }
