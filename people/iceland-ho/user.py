430b032b2d78e3fce2a15b28b1c6f00ae026d892
The Map Group <admin@the-map-group.top>
2024-07-25 03:06:47 -0300

    [auto] Updated map for member 'hó'

--- people/iceland-ho/user.py
+++ people/iceland-ho/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Hugi Ólafsson',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/59509464@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/iceland-ho/',
-  'markers': 2980,
-  'photos': 2982
+  'markers': 2981,
+  'photos': 2983
 }
