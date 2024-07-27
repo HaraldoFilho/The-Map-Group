d4d34978b5158e8cfeecfc6369cc82c0d0806952
The Map Group <admin@the-map-group.top>
2024-07-27 03:52:25 -0300

    Removed member '188891681@N08'

--- people/188891681@N08/user.py
+++
@@ -1,9 +0,0 @@
-user_info = {
-  'id': '188891681@N08',
-  'alias': '188891681@N08',
-  'name': 'kundun59',
-  'avatar': 'https://live.staticflickr.com/5674/buddyicons/188891681@N08_r.jpg',
-  'url': 'https://www.flickr.com/photos/188891681@N08/',
-  'markers': 0,
-  'photos': 0
-}
