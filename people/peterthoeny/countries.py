76eb614a0153365221e9aa2a1728b24f38615b1a
The Map Group <admin@the-map-group.top>
2024-07-29 03:25:08 -0300

    [auto] Updated map for member 'PeterThoeny'

--- people/peterthoeny/countries.py
+++ people/peterthoeny/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'CH': ['Switzerland', 225, 385],
-  'JP': ['Japan', 113, 131],
   'US': ['United States of America', 1638, 3157],
+  'GL': ['Greenland', 5, 5],
+  'IT': ['Italy', 34, 48],
+  'CN': ['South China Sea Islands (disputed)', 71, 95],
+  'NO': ['Norway', 1, 1],
   'EE': ['Estonia', 1, 1],
   'DE': ['Germany', 2, 3],
-  'CN': ['South China Sea Islands (disputed)', 71, 95],
-  'CA': ['Canada', 5, 5],
-  'NO': ['Norway', 1, 1],
   'AT': ['Austria', 6, 9],
-  'LI': ['Liechtenstein', 4, 5],
-  'GL': ['Greenland', 5, 5],
   'GB': ['United Kingdom', 17, 34],
+  'JP': ['Japan', 114, 132],
+  'CA': ['Canada', 5, 5],
+  'LI': ['Liechtenstein', 4, 5],
   'IS': ['Iceland', 66, 97],
-  'UA': ['Ukraine', 1, 1],
-  'IT': ['Italy', 34, 48]
+  'CH': ['Switzerland', 225, 385],
+  'UA': ['Ukraine', 1, 1]
 }
--- people/peterthoeny/user.py
+++ people/peterthoeny/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Peter Thoeny - Quality HDR Pho',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/98786299@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/peterthoeny/',
-  'markers': 2190,
-  'photos': 3978
+  'markers': 2191,
+  'photos': 3979
 }
