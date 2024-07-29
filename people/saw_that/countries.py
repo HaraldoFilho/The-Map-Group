1178384f1c9d9844df08faf0add7fd1f19b9979b
The Map Group <admin@the-map-group.top>
2024-07-29 03:27:12 -0300

    [auto] Updated map for member 'I saw_that'

--- people/saw_that/countries.py
+++ people/saw_that/countries.py
@@ -1,3 +1,3 @@
 countries_dict = {
-  'CA': ['Canada', 9, 10]
+  'CA': ['Canada', 10, 11]
 }
--- people/saw_that/user.py
+++ people/saw_that/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'I saw_that',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/88196436@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/saw_that/',
-  'markers': 9,
-  'photos': 10
+  'markers': 10,
+  'photos': 11
 }
