c5f8f183d6088b917d6b62bf4007e822308844b6
The Map Group <admin@the-map-group.top>
2024-08-03 03:12:12 -0300

    [auto] Updated map for member 'Kevin Povenz'

--- people/kevinpovenz/countries.py
+++ people/kevinpovenz/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'LC': ['Saint Lucia', 2, 2],
-  'US': ['United States', 753, 2326],
-  'SK': ['Slovakia', 3, 6],
-  'NL': ['Netherlands', 2, 2],
   'GP': ['Guadeloupe', 1, 2],
-  'BB': ['Barbados', 1, 1]
+  'US': ['United States', 753, 2327],
+  'BB': ['Barbados', 1, 1],
+  'SK': ['Slovakia', 3, 6],
+  'LC': ['Saint Lucia', 2, 2],
+  'NL': ['Netherlands', 2, 2]
 }
