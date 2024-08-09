f7e75f39877b9993dd7f5fc2e31e78bfe480e0e6
The Map Group <admin@the-map-group.top>
2024-08-09 03:27:18 -0300

    [auto] Updated map for member 'CoronaViking (mountain photography)'

--- people/starcitizen/countries.py
+++ people/starcitizen/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'IT': ['Italy', 70, 71],
-  'AR': ['Argentina', 226, 230],
-  'FR': ['France', 193, 195],
-  'CL': ['Chile', 88, 98],
+  'NZ': ['New Zealand', 241, 256],
+  'AR': ['Argentina', 227, 231],
   'NO': ['Norway', 817, 849],
   'AD': ['Andorra', 626, 634],
-  'CH': ['Switzerland', 6, 6],
-  'NZ': ['New Zealand', 241, 256],
-  'ES': ['Spain', 1150, 1176]
+  'ES': ['Spain', 1150, 1176],
+  'FR': ['France', 193, 195],
+  'IT': ['Italy', 70, 71],
+  'CL': ['Chile', 88, 98],
+  'CH': ['Switzerland', 6, 6]
 }
--- people/starcitizen/user.py
+++ people/starcitizen/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'CoronaViking (mountain photogr',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/169246257@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/starcitizen/',
-  'markers': 3417,
-  'photos': 3515
+  'markers': 3418,
+  'photos': 3516
 }
